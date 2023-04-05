426bd8fe81fffa3b1be9a262ed0d652ffdccf0ab
EdV4H <32019044+EdV4H@users.noreply.github.com>
2023-04-05 10:26:20 +0900

    :seeding: add: temp files

--- src/features/Whiteboard/view/index.tsx
+++ src/features/Whiteboard/view/index.tsx
@@ -1,25 +1,65 @@
-import { Sprite, Stage } from "@pixi/react";
+import { Graphics, Stage } from "@pixi/react";
+import * as PIXI from "pixi.js";
+import { useCallback, useMemo, useState } from "react";
+
+PIXI.BaseTexture.defaultOptions.scaleMode = PIXI.SCALE_MODES.NEAREST;
 
 export const Template: React.FC = () => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [position, setPosition] = useState({ x: 190, y: 80 });
+  const offset = useMemo(() => new PIXI.Point(), []);
+
+  const handleDragStart = useCallback((e: PIXI.FederatedPointerEvent) => {
+    const dragTarget = e.target as PIXI.Graphics;
+    dragTarget.toLocal(e.global, undefined, offset);
+    console.log("drag start", e.global, offset);
+    offset.x *= dragTarget.scale.x;
+    offset.y *= dragTarget.scale.y;
+    console.log("drag start end", e.global, offset);
+    setIsDragging(true);
+  }, []);
+
+  const handleMove = useCallback(
+    (e: PIXI.FederatedPointerEvent) => {
+      if (isDragging) {
+        setPosition({ x: e.global.x - offset.x, y: e.global.y - offset.y });
+      }
+    },
+    [isDragging]
+  );
+
+  const handleDragEnd = useCallback((e: PIXI.FederatedPointerEvent) => {
+    console.log("drag end", e.global, offset);
+    setIsDragging(false);
+  }, []);
+
+  const drawRect = useCallback(
+    (g: PIXI.Graphics) => {
+      g.clear();
+      g.beginFill(0x000000);
+      g.drawRect(position.x, position.y, 150, 150);
+      g.endFill();
+    },
+    [position]
+  );
+
   return (
     <Stage
       width={window.innerWidth}
       height={window.innerHeight}
       options={{
         backgroundAlpha: 0,
+        eventMode: "static",
       }}
     >
-      <Sprite
-        image="https://pixijs.io/pixi-react/img/bunny.png"
-        pointerdown={() => console.log("pointerdown")}
-        pointerup={() => console.log("pointerup")}
-        pointerupoutside={() => console.log("pointerupoutside")}
-        pointermove={() => console.log("pointermove")}
+      <Graphics
+        draw={drawRect}
+        eventMode="static"
+        pointerdown={handleDragStart}
+        pointerup={handleDragEnd}
+        pointerupoutside={handleDragEnd}
+        pointermove={handleMove}
         anchor={0.5}
-        position={{
-          x: window.innerWidth / 2,
-          y: window.innerHeight / 2,
-        }}
       />
     </Stage>
   );
